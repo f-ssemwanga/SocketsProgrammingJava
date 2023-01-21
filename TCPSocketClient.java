dcb7ab30579937951ffa9c020e73e316e658c83c
Francis Ssemwanga <dssales@gmx.com>
2023-01-21 08:08:45 +0000

    Added coded TCP Socket Client Added and started coding TCP Socket Server

---
+++ TCPSocketClient.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+
+/** @noinspection InfiniteLoopStatement*/
+public class TCPSocketClient {
+    public static void main(String [] args) throws IOException {
+        //create a socket and handle the IOException
+        //localhost because server and client are on the same machine
+        Socket sk = new Socket("localhost", 9999);
+
+        //create a data output stream converts raw data from client to server into streams
+        //pass the socket object to send the data
+        //buffer reader takes input from the the user
+        DataOutputStream dout = new DataOutputStream(sk.getOutputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true){
+            //while loop will allow data to be sent continuously between client and server
+            String userInput = br.readLine(); // take user input
+            dout.writeUTF(userInput); //write userinput into the stream object
+        }
+    }
+}
